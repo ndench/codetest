78d499fa73ba3f0c5014139710cc18d086caf575
Nathan Dench <ndenc2@gmail.com>
2017-09-17 17:30:34 +1000

    refactor(drawPage): Rearrange columsn

--- src/views/site/draws.php
+++ src/views/site/draws.php
@@ -31,92 +31,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="row">
             <div class="col-lg-6 col-lg-offset-3">
-                <h2>Lottery Detail</h2>
-
-                <?php
-                echo DetailView::widget([
-                    'model' => $draw,
-                    'attributes' => [
-                        [
-                            'attribute' => 'name',
-                            'value' => $draw->getName(),
-                        ],
-                        [
-                            'attribute' => 'description',
-                            'value' => $draw->getDescription(),
-                        ],
-                        [
-                            'attribute' => 'number',
-                            'value' => $draw->getNumber(),
-                        ],
-                        [
-                            'attribute' => 'drawStarts',
-                            'value' => $draw->getStart(),
-                            'format' => 'datetime',
-                        ],
-                        [
-                            'attribute' => 'drawEnds',
-                            'value' => $draw->getEnd(),
-                            'format' => 'datetime',
-                        ],
-                        [
-                            'attribute' => 'Terms',
-                            'value' => $draw->getTermsUrl(),
-                        ],
-                    ],
-                ]);
-                ?>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-6 col-lg-offset-3">
-                <h2>Prize Detail</h2>
-                <small>What we're all here for</small>
-
-                <?php
-                echo DetailView::widget([
-                    'model' => $prize,
-                    'attributes' => [
-                        [
-                            'attribute' => 'cardTitle',
-                            'value' => $prize->getCardTitle(),
-                        ],
-                        [
-                            'attribute' => 'name',
-                            'value' => $prize->getName(),
-                        ],
-                        [
-                            'attribute' => 'description',
-                            'value' => $prize->getDescription(),
-                        ],
-                        [
-                            'attribute' => 'valueIsExact',
-                            'value' => function (RafflePrize $model) {
-                                if ($model->isExact()) {
-                                    return \yii\bootstrap\Html::icon('ok', ['style' => 'color:green']);
-                                }
-
-                                return \yii\bootstrap\Html::icon('remove', ['style' => 'color:red']);
-                            },
-                            'format' => 'raw',
-                        ],
-                        [
-                            'attribute' => 'image',
-                            'value' => $prize->getImage(),
-                        ],
-                        [
-                            'attribute' => 'value',
-                            'value' => $prize->getValue(),
-                        ],
-                    ],
-                ]);
-                ?>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-6 col-lg-offset-3">
                 <h2>Buy Some Tickets!</h2>
                 <small>Help out a charity and feel good about yourself while you do it!</small>
 
@@ -170,6 +84,89 @@ $this->params['breadcrumbs'][] = $this->title;
                         ]
                     ]
                 ])
+                ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Lottery Detail</h2>
+
+                <?php
+                echo DetailView::widget([
+                    'model' => $draw,
+                    'attributes' => [
+                        [
+                            'attribute' => 'name',
+                            'value' => $draw->getName(),
+                        ],
+                        [
+                            'attribute' => 'description',
+                            'value' => $draw->getDescription(),
+                        ],
+                        [
+                            'attribute' => 'number',
+                            'value' => $draw->getNumber(),
+                        ],
+                        [
+                            'attribute' => 'drawStarts',
+                            'value' => $draw->getStart(),
+                            'format' => 'datetime',
+                        ],
+                        [
+                            'attribute' => 'drawEnds',
+                            'value' => $draw->getEnd(),
+                            'format' => 'datetime',
+                        ],
+                        [
+                            'attribute' => 'Terms',
+                            'value' => $draw->getTermsUrl(),
+                        ],
+                    ],
+                ]);
+                ?>
+            </div>
+
+            <div class="col-lg-6">
+                <h2>Prize Detail</h2>
+
+                <?php
+                echo DetailView::widget([
+                    'model' => $prize,
+                    'attributes' => [
+                        [
+                            'attribute' => 'cardTitle',
+                            'value' => $prize->getCardTitle(),
+                        ],
+                        [
+                            'attribute' => 'name',
+                            'value' => $prize->getName(),
+                        ],
+                        [
+                            'attribute' => 'description',
+                            'value' => $prize->getDescription(),
+                        ],
+                        [
+                            'attribute' => 'valueIsExact',
+                            'value' => function (RafflePrize $model) {
+                                if ($model->isExact()) {
+                                    return \yii\bootstrap\Html::icon('ok', ['style' => 'color:green']);
+                                }
+
+                                return \yii\bootstrap\Html::icon('remove', ['style' => 'color:red']);
+                            },
+                            'format' => 'raw',
+                        ],
+                        [
+                            'attribute' => 'image',
+                            'value' => $prize->getImage(),
+                        ],
+                        [
+                            'attribute' => 'value',
+                            'value' => $prize->getValue(),
+                        ],
+                    ],
+                ]);
                 ?>
             </div>
         </div>
